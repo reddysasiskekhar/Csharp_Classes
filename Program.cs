db902f7e7723aeba0dd0fd3a5a78c69e7d886115
osasisekharreddy <sasisekhar999@gmail.com>
2024-10-28 02:51:03 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Classes
+{
+    internal class Program
+    {
+        // we have three types of classes
+        // constroctor
+        // methods
+        // propory
+        static void Main(string[] args)
+        {
+        }
+    }
+
+}
